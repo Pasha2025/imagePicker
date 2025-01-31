f6311111cf65b891f2a4b8192024730c0a9f588b
Your Name <you@example.com>
2025-01-31 20:58:52 +0530

    first commit

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({super.key});
+// class DashboardScreen extends StatelessWidget {
+//   const DashboardScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     throw Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Dashboard Screen"),
+//       ),
+//       body: Column(
+//         children: [ElevatedButton(onPressed: onPressed, child: child)],
+//       ),
+//     );
+//   }
+// }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -17,7 +18,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   );
   bool visiblePassword = false;
   XFile? pickedXFile;
-
+  var _formKey = GlobalKey<FormState>();
   @override
   void initState() {
     //calls only ones
@@ -36,103 +37,137 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        centerTitle: true,
-        title: Text("Registration Screen"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            CachedNetworkImage(
-                height: 200,
-                width: 200,
-                imageUrl:
-                    "https://th.bing.com/th/id/OIP.XAuYSVkhX9E8uZNJ2ukLvwAAAA?rs=1&pid=ImgDetMain"),
-            Text(
-              "welcome To The App , Create your account enjoy the Shopping ",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Stack(
-              children: [
-                pickedXFile != null
-                    ? CircleAvatar(
-                        radius: 60,
-                        backgroundImage: FileImage(File(pickedXFile!.path)),
-                      )
-                    : CircleAvatar(
-                        radius: 60,
-                        backgroundImage:
-                            CachedNetworkImageProvider(defaultImage)),
-                // const CircleAvatar(
-                //     radius: 60,
-                // backgroundImage: CachedNetworkImageProvider(
-                //     "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Picture.png")),
-                Positioned(
-                    bottom: 0,
-                    right: 8,
-                    child: InkWell(
-                        onTap: () async {
-                          ImagePicker imagePicker = ImagePicker();
-                          pickedXFile = await imagePicker.pickImage(
-                              source: ImageSource.gallery,
-                              imageQuality: 50,
-                              preferredCameraDevice: CameraDevice.front);
-                          if (pickedXFile != null) {
-                            print("image picked: ${pickedXFile!.path}");
-                            setState(() {});
-                          } else {
-                            print("image not picked ");
-                          }
-                        },
-                        child: Icon(Icons.photo_camera, size: 32)))
-              ],
-            ),
-            // name field
-            textFieldDefaultDap,
-            TextFormField(
-              maxLength: 50,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                  hintText: "Enter your Email",
-                  label: Text("Email"),
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder()),
-            ),
-            textFieldDefaultDap,
-            TextFormField(
-              obscureText: visiblePassword,
-              maxLength: 10,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                  hintText: "Enter your password",
-                  label: Text("password"),
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          visiblePassword = !visiblePassword;
-                        });
-                      },
-                      child: Icon(visiblePassword
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility_outlined)),
-                  border: OutlineInputBorder()),
-            ),
-            textFieldDefaultDap,
-            ElevatedButton(onPressed: () {}, child: const Text("Registration")),
-            TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Already have account..?",
-                  style: TextStyle(color: Colors.red),
-                ))
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+          title: Text("Registration Screen"),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  CachedNetworkImage(
+                      height: 200,
+                      width: 200,
+                      imageUrl:
+                          "https://th.bing.com/th/id/OIP.XAuYSVkhX9E8uZNJ2ukLvwAAAA?rs=1&pid=ImgDetMain"),
+                  Text(
+                    "welcome To The App , Create your account enjoy the Shopping ",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Stack(
+                    children: [
+                      pickedXFile != null
+                          ? CircleAvatar(
+                              radius: 60,
+                              backgroundImage:
+                                  FileImage(File(pickedXFile!.path)),
+                            )
+                          : CircleAvatar(
+                              radius: 60,
+                              backgroundImage:
+                                  CachedNetworkImageProvider(defaultImage)),
+                      // const CircleAvatar(
+                      //     radius: 60,
+                      // backgroundImage: CachedNetworkImageProvider(
+                      //     "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Picture.png")),
+                      Positioned(
+                          bottom: 0,
+                          right: 8,
+                          child: InkWell(
+                              onTap: () async {
+                                ImagePicker imagePicker = ImagePicker();
+                                pickedXFile = await imagePicker.pickImage(
+                                    source: ImageSource.gallery,
+                                    imageQuality: 50,
+                                    preferredCameraDevice: CameraDevice.front);
+                                if (pickedXFile != null) {
+                                  print("image picked: ${pickedXFile!.path}");
+                                  setState(() {});
+                                } else {
+                                  print("image not picked ");
+                                }
+                              },
+                              child: Icon(Icons.photo_camera, size: 32)))
+                    ],
+                  ),
+                  // name field
+                  textFieldDefaultDap,
+                  TextFormField(
+                    maxLength: 50,
+                    keyboardType: TextInputType.text,
+                    onChanged: (value) {
+                      print("value : $value");
+                      _formKey.currentState.validate();
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length < 3) {
+                        return 'please enter valid email address ';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Enter your Email",
+                        label: Text("Email"),
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder()),
+                  ),
+                  textFieldDefaultDap,
+                  TextFormField(
+                    obscureText: visiblePassword,
+                    maxLength: 10,
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length < 8) {
+                        return 'please enter the currect password ';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Enter your password",
+                        label: Text("password"),
+                        prefixIcon: Icon(Icons.password),
+                        suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                visiblePassword = !visiblePassword;
+                              });
+                            },
+                            child: Icon(visiblePassword
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined)),
+                        border: OutlineInputBorder()),
+                  ),
+                  textFieldDefaultDap,
+                  ElevatedButton(
+                      onPressed: () {
+                        if (pickedXFile == null) {
+                          print("please select image");
+                          Fluttertoast.showToast(msg: "please select image");
+                        } else if (_formKey.currentState!.validate()) {
+                          print("processing your request");
+                          Fluttertoast.showToast(
+                              msg: "Account create Sucssesfully succesfully");
+                        } else {
+                          print("Something went wrong");
+                        }
+                      },
+                      child: const Text("Registration")),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Already have account..?",
+                        style: TextStyle(color: Colors.red),
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 
   @override
